52b6e5548352baffefadb7c1c07f20531224e11f
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-21 06:42:02 +0000

    DBStorage - States and cities

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,18 @@
 import uuid
 from datetime import datetime
 
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -31,6 +40,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+	storage.new(self)
         storage.save()
 
     def to_dict(self):
