a158a599ba7a7d4c04d549ff55760bfa38f8a158
amanabay <amanuelabay22@gmail.com>
2023-07-10 03:47:23 +0300

    Add check condition

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,6 +2,7 @@
 """ Fabric script that generated .tgz archive of web_static folder"""
 from fabric.api import local
 from datetime import datetime
+from os.path import isdir
 
 
 def do_pack():
@@ -9,7 +10,8 @@ def do_pack():
          Generates a .tgz archive from the contents of the web_static folder
          of AirBnB Clone.
     """
-    local("mkdir -p versions")
+    if isdir("versions") is False:
+        local("mkdir -p versions")
 
     now = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_name = "web_static_{}.tgz".format(now)
