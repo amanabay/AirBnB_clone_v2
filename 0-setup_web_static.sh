9502cf066b65b517ab71d74b760aa7d3c357103c
amanabay <amanuelabay22@gmail.com>
2023-07-10 03:49:53 +0300

    Fix sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,12 +7,12 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 
-echo "<html><head></head><body>This is a test</body></html>" | sudo tee /data/web_static/releases/test/index.html
+echo "<html><head></head><body> test, test 1 2 3</body></html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 sudo chown -hR ubuntu:ubuntu /data/
 
-sudo sed -i '42i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 sudo service nginx restart
