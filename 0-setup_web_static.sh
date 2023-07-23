69047c4c2d7978a3a0d0d80e8ef8491d49dbb7f0
amanabay <amanuelabay22@gmail.com>
2023-07-23 12:54:16 +0300

    Fix setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,12 +7,33 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 
-echo "<html><head></head><body> test, test 1 2 3</body></html>" | sudo tee /data/web_static/releases/test/index.html
+echo "Test test 1, 2, 3" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 sudo chown -hR ubuntu:ubuntu /data/
 
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-sudo service nginx start
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://my80stv.com;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" | sudo tee /etc/nginx/sites-available/default > /dev/null
+
+sudo nginx -s reload
