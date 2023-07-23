715568d656516945f9790ab9261288ad761dbaf1
amanabay <amanuelabay22@gmail.com>
2023-07-24 00:42:58 +0300

    Add number_odd_or_even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -2,7 +2,7 @@
 """
     Starts web application listening on 0.0.0.0, port 5000
 """
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -48,6 +48,23 @@ def number(n):
         Displays (n is a number) if n is an integer
     """
     return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+        Display a HTML page only if n is an integer
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """
+        Displays a HTML page only if n is an integer:
+        H1 tag: “Number: n is even|odd”
+    """
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == "__main__":
