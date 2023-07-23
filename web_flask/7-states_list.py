a10ff465298a4b7beb4eff43f23e563fd983f602
amanabay <amanuelabay22@gmail.com>
2023-07-24 02:13:33 +0300

    List states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+    Starts web application listening on 0.0.0.0, port 5000
+    routes:
+        /states_list: list of all states in storage models
+"""
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+        Displays page with all State objects sorted by name(ASC)
+    """
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
